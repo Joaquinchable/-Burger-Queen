7564d7cd9791db372fd1b66d225808480635ce81
Joaquin Chable <joaquinchable9@gmail.com>
2020-08-05 06:28:16 -0500

    integrando BrowserReouter Switch Route y declarando las rutas a App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
-
-import React from 'react'
-
+import React from 'react';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import PageAlmuerzoCena from './Pages/PageAlmuerzoCena';
+import Pagedesayuno from './Pages/PageDesayuno';
 function App(){
 
         return (
-            <div>
 
-             <h1>Hola Mundo </h1>
-                
+            <BrowserRouter>
+            <Switch>
+            <Route exact path= '/menudesayunos' component = {Pagedesayuno}/>
+            <Route exact path= '/menurestodeldia' component = {PageAlmuerzoCena}/>
 
-
-            </div>
+            </Switch>
+            
+          </BrowserRouter>
+           
         )
     }
 
