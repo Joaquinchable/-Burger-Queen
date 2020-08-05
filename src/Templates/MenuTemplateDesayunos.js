6564a8f3b1ea16dc95fd14322e8095e1ddba0f74
Joaquin Chable <joaquinchable9@gmail.com>
2020-08-05 06:06:46 -0500

    integrando Templates

---
+++ src/Templates/MenuTemplateDesayunos.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+import BtnsMenus from './../Organisms/SectionBtnMenu';
+import Desayunos from './../Organisms/SectionDesayunos';
+
+function MenuTemplateDesayunos (props){
+
+   return(
+       
+        <div className="Template-container"> 
+
+       
+
+        <BtnsMenus/>
+
+        <Desayunos/>
+      
+        
+        </div>
+ 
+
+    
+   )
+
+
+
+} 
+
+
+export default MenuTemplateDesayunos;
---
+++ src/Templates/MenuTemplatesAlmCena.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import BtnsMenus from './../Organisms/SectionBtnMenu';
+import AlmCena from './../Organisms/SectionAlmCena';
+
+function MenuTemplateDesayunos (props){
+
+   return(
+       
+        <div className="Template-container"> 
+
+       
+
+        <BtnsMenus/>
+        <AlmCena/>
+      
+        
+        </div>
+ 
+
+    
+   )
+
+
+
+} 
+
+
+export default MenuTemplateDesayunos;
